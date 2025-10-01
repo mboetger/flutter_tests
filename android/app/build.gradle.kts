23984030cfe6251062fc89ec500ba875ca22636a
Matt Boetger <boetger@google.com>
2025-10-01 13:41:20 -0700

    still works without abiFilters

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,10 +29,6 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-
-        ndk {
-            abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a", "x86_64"))
-        }
     }
 
     buildTypes {
