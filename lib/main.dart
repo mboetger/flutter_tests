d8f9cfaf9af47719a46369be01953708b672f785
Matt Boetger <boetger@google.com>
2025-10-06 09:41:28 -0700

    add repro

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // I discovered this bug when I wanted to make the color of the system navigation bar reflect the
+  // modern android behavior: showing the scaffold color.
+  // This is why I made the navigation bar transparent here. Though, this line is not necesarry
+  // in the reproduction of this bug.
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+    ),
+  );
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Keyboard Stutter')),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+          child: Column(
+            children: [
+              Wrap(
+                spacing: 10,
+                children: [
+                  // Here is the deal. Setting any SystemUIMode other than the default one induces
+                  // this bug.
+                  ElevatedButton(
+                    onPressed: () {
+                      SystemChrome.setEnabledSystemUIMode(
+                        SystemUiMode.manual,
+                        overlays: SystemUiOverlay.values,
+                      );
+                    },
+                    child: const Text('Default'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      SystemChrome.setEnabledSystemUIMode(
+                        SystemUiMode.edgeToEdge,
+                      );
+                    },
+                    child: const Text('Edge to Edge'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      SystemChrome.setEnabledSystemUIMode(
+                        SystemUiMode.immersive,
+                      );
+                    },
+                    child: const Text('Immersive'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      SystemChrome.setEnabledSystemUIMode(
+                        SystemUiMode.immersiveSticky,
+                      );
+                    },
+                    child: const Text('Immersive Sticky'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      SystemChrome.setEnabledSystemUIMode(
+                        SystemUiMode.leanBack,
+                      );
+                    },
+                    child: const Text('Leanback'),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Align(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText:
+                      'Focus on this TextField and observe the bottom.',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox.fromSize(
+                size: const Size.fromHeight(50),
+                child: const ColoredBox(
+                  color: Colors.red,
+                  child: Center(
+                    child: Text('Observe the behavior of this bar.'),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
